6764ee8cc4f93abeb7d25745fe44d652c2fbda1d
sandunperera <sandun.eternal@gmail.com>
2021-04-30 23:06:03 +0530

    development

---
+++ seeders/20210430171040-course.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert('course', [{
+            name: 'BIT - UCSC',
+            description: 'BIT - university of colombo',
+            course_fee: 350000.00,
+            status: 'enabled',
+            created_at: '2021-04-30',
+            updated_at: '2021-04-30'
+        }])
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        return queryInterface.bulkDelete('course', null, {})
+    }
+};
