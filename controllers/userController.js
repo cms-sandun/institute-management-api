f95368abe0f4bd7359e09be7923a336b1aa396f9
sandunperera <sandun.eternal@gmail.com>
2021-05-09 07:12:32 +0530

    development

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 import userRepository from '../repositories/userRepository';
+import employeeRepository from "../repositories/employeeRepository";
+import emailHelper from "../helpers/emailHelper";
 
 class UserController {
 
@@ -9,12 +11,12 @@ class UserController {
 
             const user = await userRepository.findByUserNameAndPwd(userName, pwd)
 
-            if(user){
+            if (user) {
                 res.status(200).send({
                     'success': true,
                     'data': user
                 });
-            }else{
+            } else {
                 res.status(400).send({
                     'success': false,
                     'msg': 'Invalid User'
@@ -31,6 +33,17 @@ class UserController {
 
     async saveUser(req, res) {
         try {
+
+            // Check for duplicate user name
+            let existingUser = await userRepository.findByUserNameAndPwd(req.body.name, req.body.password);
+            if(existingUser){
+                res.status(400).send({
+                    'success': false,
+                    'msg': "Username already exists."
+                });
+                return
+            }
+
             let user = {};
             // Get values from request body
 
@@ -38,10 +51,26 @@ class UserController {
             // Need to encrypt the password
             user.password = req.body.password;
             user.user_type = req.body.user_type;
+            let employee_id = req.body.employee_id;
 
             let newUser = await userRepository.create(user);
 
-            // update user id in employee table
+            // Send email to the employee
+            let employee = await employeeRepository.findById(employee_id)
+            const emaildata = {
+                email: employee.email,
+                subject: "User account created",
+                text: `
+                                <p>Hello ${employee.first_name}</p>
+                                <p>New user account has been created.</p>
+                                <h5>details</h5>
+                                <p>User Name : ${user.username}</p>
+                                <p>Pwd : ${user.password}</p>
+                                <a href="http://localhost:3000/login">Login</a>
+                            `
+            }
+
+            await emailHelper.sendTextEmail(emaildata)
 
             res.status(200).send({
                 'success': true,
@@ -87,7 +116,7 @@ class UserController {
         }
     }
 
-        async updateUser(req, res) {
+    async updateUser(req, res) {
         try {
             let user = {};
             let userId = req.params.id;
